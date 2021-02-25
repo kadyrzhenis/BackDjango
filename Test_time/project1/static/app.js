79a1317d177e2bc73f43164cd7a8fef8386d4279
kadyrzhenis <kadyrkulovzhenis000509@gmail.com>
2021-02-25 21:10:33 +0600

    fghjkl

---
+++ Test_time/project1/static/app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#orders_app',
+    data:{
+        orders:[]
+    },
+    created: function () {
+             const vm=this;
+             axios.get('/api/orders/')
+                 .then(function (response) {
+                      vm.orders=response.data
+                 })
+    }
+})
