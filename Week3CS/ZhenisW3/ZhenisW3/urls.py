3b88b442066d133eb8b821f59ae5f5263ce6bd9a
kadyrzhenis <kadyrkulovzhenis000509@gmail.com>
2021-02-25 22:09:40 +0600

    aqw

--- Week3CS/ZhenisW3/ZhenisW3/urls.py
+++ Week3CS/ZhenisW3/ZhenisW3/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from main.views import index
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
