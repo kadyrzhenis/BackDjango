448ab56057e3ae44a5b07f4076e8bb2301746ddc
kadyrzhenis <kadyrkulovzhenis000509@gmail.com>
2021-03-04 02:32:15 +0600

    change

--- TodoSecond/Todo2/main/views.py
+++ TodoSecond/Todo2/main/views.py
@@ -4,11 +4,11 @@ from main.models import Todo
 
 
 def todos_list(request):
-    return render(request, 'todos_list.html', {'todos': Todo.objects.all()})
+    return render(request, 'todos_list.html', {'todos': Todo.objects.filter(mark=False)})
 
 
 def completed_todos_list(request):
-    return render(request, 'completed_todos_list.html', {'todos': Todo.objects.all()})
+    return render(request, 'completed_todos_list.html', {'todos': Todo.objects.filter(mark=True)})
 
 
 def index_page(request):
